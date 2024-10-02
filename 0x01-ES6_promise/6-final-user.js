4cbb51c30dd754cd3e71e5ffa542d891729fc18a
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-03 00:12:51 +0300

    0x01-ES6_promise task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,15 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const array = [];
-
-      results.forEach((result) => {
-        array.push({
-          status: result.status,
-          value: result,
-        });
-      });
-      return array;
-    });
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.value,
+    })));
 }
