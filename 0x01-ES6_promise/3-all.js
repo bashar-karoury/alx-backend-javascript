0c486bfaf535818d1fe7f6d7003605f249815028
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-02 17:03:50 +0300

    0x01-ES6_promise task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,22 +1,24 @@
 import { uploadPhoto, createUser } from './utils.js'; // eslint-disable-line import/extensions
 
-// export default function handleProfileSignup() {
-//   Promise.all([uploadPhoto(), createUser()])
-//     .then((values) => { console.log(values[0].body, values[1].firstName, values[1].lastName); })
-//     .catch(() => { console.log('Signup system offline'); });
-// }
 export default function handleProfileSignup() {
-  let photo;
-  let firstName;
-  let lastName;
-  uploadPhoto()
-    .then((value) => {
-      photo = value.body;
-      return createUser();
-    }).then((value) => {
-      firstName = value.firstName;
-      lastName = value.lastName;
-      console.log(photo, firstName, lastName);
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => { console.log('Signup system offline'); });
 }
+// export default function handleProfileSignup() {
+//   let photo;
+//   let firstName;
+//   let lastName;
+//   uploadPhoto()
+//     .then((value) => {
+//       photo = value.body;
+//       return createUser();
+//     }).then((value) => {
+//       firstName = value.firstName;
+//       lastName = value.lastName;
+//       console.log(photo, firstName, lastName);
+//     })
+//     .catch(() => { console.log('Signup system offline'); });
+// }
