b9845eb5361f88c0edd3ad1e8efc585636cd3cea
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-02 14:48:11 +0300

    0x01-ES6_promise task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => new Error())
+  let result;
+  promise.then(() => { result = { status: 200, body: 'Success' }; })
+    .catch(() => { result = new Error(); })
     .finally(() => console.log('Got a response from the API'));
+  return result;
 }
