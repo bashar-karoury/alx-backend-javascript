a84ca5251a47d2512172d6b10ec1f9640b75f3b4
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-03 17:22:42 +0300

    7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = name;
-    this._code = code;
+    this._name = name; // eslint-disable-line 
+    this._code = code; // eslint-disable-line 
   }
 
   toString() {
-    return `[object ${this._code}]`;
+    return `[object ${this._code}]`; // eslint-disable-line 
   }
 }
