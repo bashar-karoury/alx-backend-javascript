9a144ae71b7e4d269ef5d76a54bacc5366a68538
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-03 16:25:37 +0300

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft; // eslint-disable-line 
+  }
+
+  get sqft() {
+    return this._sqft; // eslint-disable-line 
+  }
+
+  evacuationWarningMessage() { // eslint-disable-line 
+    throw (new Error('Class extending Building must override evacuationWarningMessage'));
+  }
+}
