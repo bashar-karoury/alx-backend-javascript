c46414072fa990b264721eedf0f21a01f8633a3e
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-07 13:36:20 +0300

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 export default function getListStudentIds(students, city, newGrades) {
   if (Array.isArray(students)) {
     const cityStudents = students.filter((o) => o.location === city);
-    cityStudents.forEach((student) => {
+    return cityStudents.map((student) => {
       const found = newGrades.find((entry) => entry.studentId === student.id);
-      student.grade = (found) ? found.grade : 'N/A';  // eslint-disable-line 
+      student.grade = (found) ? found.grade : 'N/A';  // eslint-disable-line
+      return student;
     });
-    return cityStudents;
   }
   return undefined;
 }
