4795dd6dd1db59a8df5402678605ec9ea4c546a8
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-07 22:44:36 +0300

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,5 +2,6 @@ export default function (set, array) {
   if (!array.length || !set.size) {
     return false;
   }
-  return (new Set(array)).isSubsetOf(set);
+  return array.every((element) => set.has(element));
+  // return (new Set(array)).isSubsetOf(set);
 }
