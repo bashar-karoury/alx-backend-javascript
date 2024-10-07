38b22de2f1da821a6dafa3031f672c6edea3ba6f
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-07 22:32:38 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
 export default function cleanSet(set, startString) {
   const array = [];
+  if (!(set instanceof Set)) {
+    return '';
+  }
   set.forEach((str) => {
     if (startString && str.startsWith(startString)) {
       array.push(str.slice(startString.length));
     }
   });
+
   return array.join('-');
 }
