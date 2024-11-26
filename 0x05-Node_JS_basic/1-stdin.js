ec7ff65aaacfe1cccc8e575e7d5f00472b1a7587
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-26 09:48:24 +0300

    Updated files: 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-console.log('Welcome to Holberton School, what is your name? ');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const studentname = data.toString();
-  console.log(`Your name is: ${studentname}`);
+  process.stdout.write(`Your name is: ${studentname}`);
 });
 process.stdin.on('close', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
