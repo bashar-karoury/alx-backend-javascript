1e817c9d5b3700673d601b94b3d00f819783bb81
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-26 16:06:50 +0300

    Updated files: 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,12 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 1245;
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
-});
+app.listen(PORT);
 
 module.exports = app;
