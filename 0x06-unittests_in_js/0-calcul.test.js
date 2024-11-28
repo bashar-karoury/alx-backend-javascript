2333f4ef1c135f5ee785599132907f5d00e5bab8
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-28 11:40:49 +0300

    Updated files: 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('Test calcul Module',function (){
-    it('test calculateNumber returns correct integars', function(){
+    it('test calculateNumber returns correct with integars', function(){
         assert.equal(calculateNumber(1, 2), 3);
+    });
+
+    it('test calculateNumber returns correct with fractions trunc', function(){
+        assert.equal(calculateNumber(1.4, 2.2), 3);
+    });
+
+    it('test calculateNumber returns correct with fractions ciel', function(){
+        assert.equal(calculateNumber(1.9, 2.6), 5);
+    });
+
+    it('test calculateNumber returns correct with negative', function(){
+        assert.equal(calculateNumber(1.5,  -1), 1);
     });
 });
